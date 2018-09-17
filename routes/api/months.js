fb130e5c0770c0f0014dcd04bfbcbe552161dfbe
Jannik Jost <jannik-jost@gmx.de>
2018-09-17 18:58:36 +0200

    added http get for a specific month

--- routes/api/months.js
+++ routes/api/months.js
@@ -3,12 +3,24 @@ const router = express.Router();
 
 const Month = require('../../models/Month');
 
+//GET
 router.get('/', (req, res) => {
     Month.find()
         .sort({ year: 1, monthIndex: 1 })
         .then(months => res.json(months))
 });
 
+router.get('/:yearMonth', (req, res) => {
+    var param = req.params.yearMonth;
+    var year = param.substring(0, 4);
+    var month = param.substring(4, param.length);
+    
+    Month.find({ year: year, monthIndex: month })
+        .sort({ year: 1, monthIndex: 1 })
+        .then(months => res.json(months))
+});
+
+//POST
 router.post('/', (req, res) => {
     const newMonth = new Month({
         monthIndex: req.body.monthIndex,
@@ -20,10 +32,11 @@ router.post('/', (req, res) => {
         .then(month => res.json(month));
 });
 
-router.delete('/:id', (req, res) => {
-    Month.findById(req.params.id)
-        .then(month => month.remove().then(() => res.json({ success: true, deletedMonth: month })))
-        .catch(err => res.status(404).json({ success: false, error: err }));
-});
+//DELETE
+// router.delete('/:id', (req, res) => {
+//     Month.findById(req.params.id)
+//         .then(month => month.remove().then(() => res.json({ success: true, deletedMonth: month })))
+//         .catch(err => res.status(404).json({ success: false, error: err }));
+// });
 
 module.exports = router;
